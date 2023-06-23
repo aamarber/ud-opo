e17fd528e556bd6c0cba2f6500bc9becacd2b86c
Aarón Martín <aamarber@gmail.com>
2023-06-23 08:31:30 +0200

    WIP

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Sass configuration
+const { watch } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', () => {
+  return gulp.src('*/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task(
+  'default',
+  gulp.series('sass', function(cb) {
+    gulp.watch('*/*.scss', gulp.series('sass'));
+    cb();
+  })
+);
