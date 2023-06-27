84b809bcd3723f5d84ac450771f81a879ca91a35
Aarón Martín <aamarber@gmail.com>
2023-06-27 13:30:35 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,18 @@
+var date = new Date(2023, 9, 9);
+
+let selectedDates =  [new Date(2023, 10, 12)];
+
+for(let i = 0; i < 21; i++){
+    let d = new Date(date);
+
+    d.setDate(d.getDate() + i);
+
+    selectedDates.push(d);
+}
+
+$('#calendar').multiDatesPicker({
+    firstDay: 1,
+    defaultDate:"10/09/2023",
+    addDates: selectedDates,
+    numberOfMonths: [1, 2]
+});
